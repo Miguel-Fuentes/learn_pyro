73d53b24907b1e2c6dfee497c2b4aa28a4d3e1ec
Miguel Fuentes <mmfuente@udel.edu>
2020-05-04 10:40:16 -0400

    Made new infection model which takes data to make observations, tried to run SVI with data but I couldn't make it work yet, updated readme to reflect progress

--- toy_infection_model/README.md
+++ toy_infection_model/README.md
@@ -1,10 +1,18 @@
 # Toy Infection Model
 Miguel Fuentes
 Created: 5/1/2020
-Last Updates: 5/1/2020
+Last Updated: 5/4/2020
 
 ## Purpose
 The point of this project is just for me learn the probabilistic programming language Pyro and as much of the underlying theory as possible. I know nothing about epidemiology so the point isn't to create an accurate model for the trnasmission of the coronavirus. As I started copying over the examples from the Pyto ducumentation and running the code I realized that I will learn better if I try to apply all of the lessons to my own proble. I thought about what problem to model for a while and I eventually decided to model different aspects of coronavirus transmission because since I am currently in lockdown because of COVID-19 it has been on my mind
 
 ## Overview
-I plan to keep adding different models and functionality as I learn more about pyro and what kinds of things I can do with it. I will also try to keep an up to date overview of what I have done so far here. (Working o)
+I plan to keep adding different models and functionality as I learn more about pyro and what kinds of things I can do with it. I will also try to keep an up to date overview of what I have done so far here.
+
+### Sample Transmission Model
+I made a simple model for transmission. The model tries to answer this question: if someone gets an infectious disease, how many people will they go on to infect. To do this the model defines some latent variables: protocol (social distancing or normal behavior), contacted people, and infection rate. Contacted people and infection rate depend on the protocol, then the variable of interest is num infected, this depends on the contacted people and the infection rate.  
+I sample this model in the notebook model_sampling and plot some of the distributions that come out.  
+In the notebook inter_prior, I condition this model on the protocol being normal behavior then try to use VI to learn the prior distribution of the model with a guide function of the same form. For a long time this wasn't working correctly and I was confused but I just understood my mistake and added a note to the notebook explaining my mistake in the conclusion.
+
+### Observe Transmission
+Here, I tried to make an infection model which will make observations of data. The model takes data points which specifiy the protocol the onfected individual is following an the number of people they infect (not realistic to have this data I know but this isn't meant to be a serious model anyway so I'll start here and maybe get more realistic later). I try to do this by defining the latent variables contacted people and infection rate then using them to define a distribution over number of infected people and then try to maximize the probability of seeing the data based on this distribution. The form is similar to the sample transmission model but the latent variables are now all to make inference easier. In the notebook infer_mle_params I try to run inference over this but I run into an error because my loss and params end up going to nan very quickle. I am going to try to look as some more examples to understand SVI better then come back and try to fix this later because as of writing this I have no idea how to fix it. 
--- toy_infection_model/infection_models.py
+++ toy_infection_model/infection_models.py
@@ -10,21 +10,15 @@ def sample_transmission():
         Contacted people -> Poisson distribution (depends on protocol)
         Infection rate -> Beta distribution (depends on protocol)    
         Num_infected -> Poisson (based on contacted people and infection rate)
-    The bistribution is defined by the following paramaters:
-        distancing -> The probability that an individual follows social distancing [0,1]
-        normal_contact -> The exptected amound of people someone will contact following normal behavior [0, inf]
-        distance_contact -> The exptected amound of people someone will contact following social distancing [0, inf]
-        normal_ir_conc -> This will pull the infection rate from a beta distribution with concentrations (conc1, conc0) [0, inf]
-        distance_ir_conc -> This will pull the infection rate from a beta distribution with concentrations (conc1, conc0) [0, inf]
     '''
     # Determine whether sampled person will follow social distancing
-    distancing = pyro.param('distancing', torch.tensor(0.3), constraint=constraints.interval(0,1))
+    distancing = torch.tensor(0.3)
     protocol = pyro.sample('protocol', pyro.distributions.Bernoulli(distancing))
     protocol = 'normal_behavior' if protocol.item() == 1.0 else 'social_distancing'
     
     # Determine how many people they are expected to run into over the course of their transmittable period
-    normal_contact = pyro.param('normal_contact', torch.tensor(100.0), constraint=constraints.positive)
-    distance_contact = pyro.param('distance_contact', torch.tensor(30.0), constraint=constraints.positive)
+    normal_contact = torch.tensor(100.0)
+    distance_contact = torch.tensor(30.0)
     rate_contacted_people = {
         'normal_behavior': normal_contact,
         'social_distancing': distance_contact
@@ -32,10 +26,10 @@ def sample_transmission():
     contacted_people = pyro.sample('contacted_people', pyro.distributions.Poisson(rate_contacted_people))
 
     # concentrations for the 2 beta distributions
-    normal_conc0 = pyro.param('normal_conc0', torch.tensor(24.0), constraint=constraints.positive)
-    normal_conc1 = pyro.param('normal_conc1', torch.tensor(6.0), constraint=constraints.positive)
-    distance_conc0 = pyro.param('distance_conc0', torch.tensor(27.0), constraint=constraints.positive)
-    distance_conc1 = pyro.param('distance_conc1', torch.tensor(3.0), constraint=constraints.positive)
+    normal_conc0 = torch.tensor(24.0)
+    normal_conc1 = torch.tensor(6.0)
+    distance_conc0 = torch.tensor(27.0)
+    distance_conc1 = torch.tensor(3.0)
     
     # determine the infection rate of the carrier
     infection_rate_params = {
@@ -49,43 +43,44 @@ def sample_transmission():
     num_infected = pyro.sample('num_infected', pyro.distributions.Poisson(rate_num_infected))
     
     # return the sample
-    return protocol, contacted_people.item(), infection_rate.item(), num_infected.item()
+    return protocol, contacted_people, infection_rate, num_infected
 
 
 def observe_transmission(data):
     '''
     This model observes defines a prior distribution over the amount of people an infected individual will transmit to,
-    then it observes data of actual transmissions. The idea being that the paramaters can be adjusted after observing more
-    data to fit the actual distribution.
+    then it observes data of actual transmissions. This uses all continuous distributions and latent variables to make VI easier.
     It is assumed that the data will be a list of tuples, where the first value states which protocol the person follows and
     the second value is the number of people the infected individual transmitted to.
-    Tha parameters for this value are the following:
-        normal_contact -> The exptected amound of people someone will contact following normal behavior [0, inf]
-        distance_contact -> The exptected amound of people someone will contact following social distancing [0, inf]
-        normal_conc1, normal_conc0-> This will pull the infection rate from a beta distribution with concentrations (conc1, conc0)
-        distance_conc1, distance_conc0-> This will pull the infection rate from a beta distribution with concentrations (conc1, conc0)
     The latent variables for this model are the following:
         cotacted_people_i -> How many people individual i contacted
         infection_rate_i -> The infection rate of individual i
     The observations thie model makes are the following:
         num_infected_i -> how many people individual i infected
     '''
-    normal_contact = pyro.param('normal_contact', torch.tensor(100.0), constraint=constraints.positive)
-    normal_conc0 = pyro.param('normal_conc0', torch.tensor(24.0), constraint=constraints.positive)
-    normal_conc1 = pyro.param('normal_conc1', torch.tensor(6.0), constraint=constraints.positive)
+    
+    normal_contact_loc = torch.tensor(50.0)
+    normal_contact_scale = torch.tensor(10.0)
+    normal_conc0 = torch.tensor(10.0)
+    normal_conc1 = torch.tensor(10.0)
 
-    distance_contact = pyro.param('distance_contact', torch.tensor(30.0), constraint=constraints.positive)
-    distance_conc0 = pyro.param('distance_conc0', torch.tensor(27.0), constraint=constraints.positive)
-    distance_conc1 = pyro.param('distance_conc1', torch.tensor(3.0), constraint=constraints.positive)
+    distance_contact_loc = torch.tensor(25.0)
+    distance_contact_scale = torch.tensor(5.0)
+    distance_conc0 = torch.tensor(10.0)
+    distance_conc1 = torch.tensor(10.0)
+
+    num_infected_scale = pyro.param('num_infected_scale', torch.tensor(5.0), constraint=constraints.positive)
+    
     for i, datum in enumerate(data):
         protocol, num_infected = datum
         if protocol == 'normal_behavior':
-            contacted_people = pyro.sample(f'contacted_people_{i}', pyro.distributions.Poisson(normal_contact))
+            contacted_people = pyro.sample(f'contacted_people_{i}', pyro.distributions.Normal(normal_contact_loc, normal_contact_scale))
             infection_rate = pyro.sample(f'infection_rate_{i}', pyro.distributions.Beta(normal_conc1, normal_conc0))
-        elif protocol == 'social_distanicng':
-            contacted_people = pyro.sample(f'contacted_people_{i}', pyro.distributions.Poisson(distance_contact))
+        elif protocol == 'social_distancing':
+            contacted_people = pyro.sample(f'contacted_people_{i}', pyro.distributions.Normal(distance_contact_loc, distance_contact_scale))
             infection_rate = pyro.sample(f'infection_rate_{i}', pyro.distributions.Beta(distance_conc1, distance_conc0))
         else:
             raise ValueError(f'Data point {i} has protocol value {protocol}, expects values \'social_distancing\' or \'normal_behavior\'')
-        rate_num_infected = contacted_people * infection_rate
-        num_infected = pyro.sample(f'num_infected_{i}', pyro.distributions.Poisson(rate_num_infected), obs=num_infected)
+        num_infected_loc = contacted_people * infection_rate
+        num_infected = pyro.sample(f'num_infected_{i}', pyro.distributions.Normal(num_infected_loc, num_infected_scale), obs=num_infected)
+
